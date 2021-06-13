c6485a963c2c4d6ac324d58d09f92c541a179f40
Alan Mantoux <alan.mantoux@gmail.com>
2021-06-13 12:11:08 +0200

    Return FrameResult

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,18 @@ extern "C" {
     fn grab_frame(
         in_data: *const u8,
         in_size: size_t,
-        out_data: &mut *mut u8,
-        outsize: &mut size_t,
-        rotate: &mut *mut c_char,
+        out_data: *mut *mut u8,
+        outsize: *mut size_t,
+        rotate: *mut *mut c_char,
     ) -> c_int;
 }
 
-pub fn get_first_frame(video_src: &[u8]) -> Vec<u8> {
+pub struct FrameResult<'f> {
+    pub buffer: Vec<u8>,
+    pub rotation: Option<&'f str>,
+}
+
+pub fn get_first_frame(video_src: &[u8]) -> FrameResult {
     unsafe {
         let in_len = video_src.len() as size_t;
         let p_in = video_src.as_ptr();
@@ -26,10 +31,19 @@ pub fn get_first_frame(video_src: &[u8]) -> Vec<u8> {
         let mut rotate: *mut c_char = std::ptr::null_mut();
         let p_rotate = &mut rotate;
         grab_frame(p_in, in_len, p_out, &mut out_len, p_rotate);
-        let c_rotate = CStr::from_ptr(rotate).to_str().unwrap();
-        println!("Rotate is {}", c_rotate);
+
+        if rotate.is_null() {
+            return FrameResult {
+                buffer: Vec::from_raw_parts(out, out_len, out_len),
+                rotation: None,
+            };
+        }
+        let c_rotate = CStr::from_ptr(rotate).to_str();
         // From here deallocation of 'out' is handled by Vec that owns the buffer
-        Vec::from_raw_parts(out, out_len, out_len)
+        FrameResult {
+            buffer: Vec::from_raw_parts(out, out_len, out_len),
+            rotation: c_rotate.map_or(None, |s| Some(s)),
+        }
     }
 }
 
@@ -43,13 +57,14 @@ mod tests {
 
     #[test]
     fn test_grab_frame() -> std::io::Result<()> {
-        let mut f = File::open("IMG_0010.MOV")?;
+        let mut f = File::open("IMG_0007.MP4")?;
         let mut buffer: Vec<u8> = Vec::new();
 
         f.read_to_end(&mut buffer)?;
         let x = get_first_frame(&buffer);
         let mut file = File::create("in_memory.jpeg")?;
-        file.write(&x)?;
+        file.write(&x.buffer)?;
+        println!("Rotate is {}", x.rotation.unwrap_or("None"));
         Ok(())
     }
 }
