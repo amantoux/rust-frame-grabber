c2fd88ba7f63327bfffa605f9fd085c3113d8fff
Alan Mantoux <alan.mantoux@gmail.com>
2021-06-19 11:57:26 +0200

    Handle errors

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ pub struct FrameResult<'f> {
     pub rotation: Option<&'f str>,
 }
 
-pub fn get_first_frame(video_src: &[u8]) -> FrameResult {
+pub fn get_first_frame(video_src: &[u8]) -> Result<FrameResult, FrameError> {
     unsafe {
         let in_len = video_src.len() as size_t;
         let p_in = video_src.as_ptr();
@@ -30,21 +30,29 @@ pub fn get_first_frame(video_src: &[u8]) -> FrameResult {
 
         let mut rotate: *mut c_char = std::ptr::null_mut();
         let p_rotate = &mut rotate;
-        grab_frame(p_in, in_len, p_out, &mut out_len, p_rotate);
+        let res = grab_frame(p_in, in_len, p_out, &mut out_len, p_rotate);
+        if res < 0 {
+            return Err(FrameError::Default);
+        }
 
         if rotate.is_null() {
-            return FrameResult {
+            return Ok(FrameResult {
                 buffer: Vec::from_raw_parts(out, out_len, out_len),
                 rotation: None,
-            };
+            });
         }
         let c_rotate = CStr::from_ptr(rotate).to_str();
         // From here deallocation of 'out' is handled by Vec that owns the buffer
-        FrameResult {
+        Ok(FrameResult {
             buffer: Vec::from_raw_parts(out, out_len, out_len),
             rotation: c_rotate.map_or(None, |s| Some(s)),
-        }
+        })
     }
+}
+
+#[derive(Debug)]
+pub enum FrameError {
+    Default,
 }
 
 #[cfg(test)]
@@ -57,14 +65,14 @@ mod tests {
 
     #[test]
     fn test_grab_frame() -> std::io::Result<()> {
-        let mut f = File::open("IMG_0007.MP4")?;
+        let mut f = File::open("IMG_0010.MOV")?;
         let mut buffer: Vec<u8> = Vec::new();
 
         f.read_to_end(&mut buffer)?;
-        let x = get_first_frame(&buffer);
+        let x = get_first_frame(&buffer).unwrap();
         let mut file = File::create("in_memory.jpeg")?;
         file.write(&x.buffer)?;
-        println!("Rotate is {}", x.rotation.unwrap_or("None"));
+        assert_eq!(x.rotation.unwrap(), "180");
         Ok(())
     }
 }
